750ee0ba171f678bade33e32a3bfb3bc05da741d
johnkdunyo <johnkdunyo@gmail.com>
2023-06-22 04:26:35 +0000

    added cors dotenv and exprress validator

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json());
 
 //defaut route
 app.get('/', (req, res) => {
